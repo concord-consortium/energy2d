613cc8069370a558668464a84214f8b4a3b2ce9c
qxie <qxie@6e01202a-0783-4428-890a-84243c50cc2b>
2010-01-16 03:21:04 +0000

    git-svn-id: https://svn.concord.org/svn/projects/trunk/common/java/simulations/Energy2D@21668 6e01202a-0783-4428-890a-84243c50cc2b

--- src/org/concord/energy2d/view/ScalarDistributionRenderer.java
+++ src/org/concord/energy2d/view/ScalarDistributionRenderer.java
@@ -21,6 +21,7 @@ class ScalarDistributionRenderer {
 	private int w, h;
 	private float scale = 40f / 255f;
 	private short[][] rgbScale;
+	private boolean smooth = true;
 
 	ScalarDistributionRenderer(short[][] rgbScale) {
 		this.rgbScale = rgbScale;
@@ -62,25 +63,73 @@ class ScalarDistributionRenderer {
 
 		float dx = (float) m / (float) w;
 		float dy = (float) n / (float) h;
+		float x, y;
 
-		int rc = 0, gc = 0, bc = 0;
-		int ix, iy, iv;
+		int rc = 0, gc = 0, bc = 0, iv;
 		float v;
-		for (int i = 0; i < w; i++) {
-			ix = (int) (i * dx);
-			for (int j = 0; j < h; j++) {
-				iy = (int) (j * dy);
-				v = distribution[ix][iy] * scale;
-				if (v > rgbScale.length - 2)
-					v = rgbScale.length - 2;
-				else if (v < 0)
-					v = 0;
-				iv = (int) v;
-				v -= iv;
-				rc = (int) (rgbScale[iv][0] * (1 - v) + rgbScale[iv + 1][0] * v);
-				gc = (int) (rgbScale[iv][1] * (1 - v) + rgbScale[iv + 1][1] * v);
-				bc = (int) (rgbScale[iv][2] * (1 - v) + rgbScale[iv + 1][2] * v);
-				pixels[i + j * w] = (255 << 24) | (rc << 16) | (gc << 8) | bc;
+		if (smooth) {
+			int i0, j0, i1, j1;
+			float s0, s1, t0, t1;
+			for (int i = 0; i < w; i++) {
+				x = i * dx;
+				i0 = (int) x;
+				i1 = i0 + 1;
+				if (i1 > m - 1)
+					i1 = m - 1;
+				s1 = x - i0;
+				s0 = 1 - s1;
+				for (int j = 0; j < h; j++) {
+					y = j * dy;
+					j0 = (int) y;
+					j1 = j0 + 1;
+					if (j1 > n - 1)
+						j1 = n - 1;
+					t1 = y - j0;
+					t0 = 1 - t1;
+					v = (s0
+							* (t0 * distribution[i0][j0] + t1
+									* distribution[i0][j1]) + s1
+							* (t0 * distribution[i1][j0] + t1
+									* distribution[i1][j1]))
+							* scale;
+					if (v > rgbScale.length - 2)
+						v = rgbScale.length - 2;
+					else if (v < 0)
+						v = 0;
+					iv = (int) v;
+					v -= iv;
+					rc = (int) (rgbScale[iv][0] * (1 - v) + rgbScale[iv + 1][0]
+							* v);
+					gc = (int) (rgbScale[iv][1] * (1 - v) + rgbScale[iv + 1][1]
+							* v);
+					bc = (int) (rgbScale[iv][2] * (1 - v) + rgbScale[iv + 1][2]
+							* v);
+					pixels[i + j * w] = (255 << 24) | (rc << 16) | (gc << 8)
+							| bc;
+				}
+			}
+		} else {
+			int i0, j0;
+			for (int i = 0; i < w; i++) {
+				i0 = (int) (i * dx);
+				for (int j = 0; j < h; j++) {
+					j0 = (int) (j * dy);
+					v = distribution[i0][j0] * scale;
+					if (v > rgbScale.length - 2)
+						v = rgbScale.length - 2;
+					else if (v < 0)
+						v = 0;
+					iv = (int) v;
+					v -= iv;
+					rc = (int) (rgbScale[iv][0] * (1 - v) + rgbScale[iv + 1][0]
+							* v);
+					gc = (int) (rgbScale[iv][1] * (1 - v) + rgbScale[iv + 1][1]
+							* v);
+					bc = (int) (rgbScale[iv][2] * (1 - v) + rgbScale[iv + 1][2]
+							* v);
+					pixels[i + j * w] = (255 << 24) | (rc << 16) | (gc << 8)
+							| bc;
+				}
 			}
 		}
 		image.setRGB(0, 0, w, h, pixels, 0, w);
