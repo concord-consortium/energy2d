d4f62eb66a6608e6d982777677b2160f7844537c
qxie <qxie@6e01202a-0783-4428-890a-84243c50cc2b>
2011-11-15 00:38:30 +0000

    git-svn-id: https://svn.concord.org/svn/projects/trunk/common/java/simulations/Energy2D@25359 6e01202a-0783-4428-890a-84243c50cc2b

--- src/org/concord/energy2d/view/ScalarDistributionRenderer.java
+++ src/org/concord/energy2d/view/ScalarDistributionRenderer.java
@@ -60,6 +60,14 @@ class ScalarDistributionRenderer {
 	int getRGB(int x, int y) {
 		if (image == null)
 			return Color.white.getRGB();
+		if (x >= image.getWidth())
+			x = image.getWidth() - 1;
+		if (x < 0)
+			x = 0;
+		if (y >= image.getHeight())
+			y = image.getHeight() - 1;
+		if (y < 0)
+			y = 0;
 		return image.getRGB(x, y);
 	}
 
