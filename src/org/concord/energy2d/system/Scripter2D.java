6bcc7683cae6222a6de3c45570774e4045243159
qxie <qxie@6e01202a-0783-4428-890a-84243c50cc2b>
2011-11-18 02:11:59 +0000

    git-svn-id: https://svn.concord.org/svn/projects/trunk/common/java/simulations/Energy2D@25376 6e01202a-0783-4428-890a-84243c50cc2b

--- src/org/concord/energy2d/system/Scripter2D.java
+++ src/org/concord/energy2d/system/Scripter2D.java
@@ -719,6 +719,15 @@ class Scripter2D extends Scripter {
 					s2d.view.setArea(0, s2d.model.getLx(), 0, height);
 					temperatureInitializationRequested = true;
 					arrayUpdateRequested = true;
+				} else if (t[0].equalsIgnoreCase("z_heat_diffusivity")) {
+					float zHeatDiffusivity = 0;
+					try {
+						zHeatDiffusivity = Float.parseFloat(t[1]);
+					} catch (NumberFormatException e) {
+						showException(ci, e);
+						return;
+					}
+					s2d.model.setZHeatDiffusivity(zHeatDiffusivity);
 				} else if (t[0].equalsIgnoreCase("background_viscosity")) {
 					float viscosity = 0;
 					try {
