6a8c7d6e5f38c97170d70ee4c5f7b065b527e520
qxie <qxie@6e01202a-0783-4428-890a-84243c50cc2b>
2012-08-04 00:24:50 +0000

    git-svn-id: https://svn.concord.org/svn/projects/trunk/common/java/simulations/Energy2D@26138 6e01202a-0783-4428-890a-84243c50cc2b

--- src/org/concord/energy2d/view/ThermostatRenderer.java
+++ src/org/concord/energy2d/view/ThermostatRenderer.java
@@ -18,10 +18,11 @@ import org.concord.energy2d.model.Thermostat;
  */
 class ThermostatRenderer {
 
-	private Stroke stroke = new BasicStroke(2);
-	private Color color = Color.white;
+	private Stroke stroke1, stroke2;
 
 	ThermostatRenderer() {
+		stroke1 = new BasicStroke(3);
+		stroke2 = new BasicStroke(1);
 	}
 
 	void render(Thermostat t, View2D v, Graphics2D g) {
@@ -31,18 +32,25 @@ class ThermostatRenderer {
 
 		Stroke oldStroke = g.getStroke();
 		Color oldColor = g.getColor();
-		g.setStroke(stroke);
-		g.setColor(color);
+		g.setStroke(stroke1);
+		g.setColor(Color.black);
 
 		int x1 = v.convertPointToPixelX(t.getThermometer().getX());
 		int y1 = v.convertPointToPixelY(t.getThermometer().getY());
 		int x2 = v.convertPointToPixelX(t.getPowerSource().getCenter().x);
-		int y2 = v.convertPointToPixelX(t.getPowerSource().getCenter().y);
+		int y2 = v.convertPointToPixelY(t.getPowerSource().getCenter().y);
 		g.drawLine(x1, y1, x1, y2);
 		g.drawLine(x1, y2, x2, y2);
-		g.setColor(Color.lightGray);
-		g.fillOval(x1 - 3, y1 - 3, 6, 6);
-		g.fillOval(x2 - 3, y2 - 3, 6, 6);
+
+		g.setStroke(stroke2);
+		g.drawOval(x1 - 3, y1 - 3, 6, 6);
+		g.drawOval(x2 - 3, y2 - 3, 6, 6);
+
+		g.setColor(Color.white);
+		g.drawLine(x1, y1, x1, y2);
+		g.drawLine(x1, y2, x2, y2);
+		g.fillOval(x1 - 2, y1 - 2, 4, 4);
+		g.fillOval(x2 - 2, y2 - 2, 4, 4);
 
 		g.setStroke(oldStroke);
 		g.setColor(oldColor);
