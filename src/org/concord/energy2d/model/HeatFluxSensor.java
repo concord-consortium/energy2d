f092865f7762cbe27b997ac66b4f6ff283b8f435
qxie <qxie@6e01202a-0783-4428-890a-84243c50cc2b>
2012-09-04 18:34:12 +0000

    git-svn-id: https://svn.concord.org/svn/projects/trunk/common/java/simulations/Energy2D@26308 6e01202a-0783-4428-890a-84243c50cc2b

--- src/org/concord/energy2d/model/HeatFluxSensor.java
+++ src/org/concord/energy2d/model/HeatFluxSensor.java
@@ -9,7 +9,7 @@ import java.awt.geom.Rectangle2D;
 public class HeatFluxSensor extends Sensor {
 
 	public final static float RELATIVE_WIDTH = 0.036f;
-	public final static float RELATIVE_HEIGHT = 0.015f;
+	public final static float RELATIVE_HEIGHT = 0.012f;
 
 	private float angle;
 
