c5ff5b0018f2b28adbd87deb4f29b14d224c2e4b
qxie <qxie@6e01202a-0783-4428-890a-84243c50cc2b>
2013-03-16 01:46:51 +0000

    git-svn-id: https://svn.concord.org/svn/projects/trunk/common/java/simulations/Energy2D@26577 6e01202a-0783-4428-890a-84243c50cc2b

--- src/org/concord/energy2d/system/MenuBar.java
+++ src/org/concord/energy2d/system/MenuBar.java
@@ -836,6 +836,7 @@ class MenuBar extends JMenuBar {
 		examples.put("Natural Convection with Different Temperatures", "models/natural-convection-temperature.e2d");
 		examples.put("Comparing Natural Convection and Conduction", "models/compare-convection-conduction.e2d");
 		examples.put("Comparing Forced Convection and Conduction", "models/forced-convection.e2d");
+		examples.put("Exploring Forced Convection", "models/forced-convection1.e2d");
 		examples.put("Stack Effect", "models/stack-effect.e2d");
 		createMenu(box, subMenu, examples);
 
